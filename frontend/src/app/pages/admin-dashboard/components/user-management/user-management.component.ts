b70d8a95be673f32d1613823a20ffed16b38e99e
Sami <samiboulsnane68@gmail.com>
2024-12-31 17:54:50 +0100

    ajustements sur la liste des utilisateurs

--- frontend/src/app/pages/admin-dashboard/components/user-management/user-management.component.ts
+++ frontend/src/app/pages/admin-dashboard/components/user-management/user-management.component.ts
@@ -11,6 +11,10 @@ import {User} from "../../../../models/User";
 })
 export class UserManagementComponent implements OnInit {
   users:User[]=[];
+  filteredUsers: User[] = [];
+  searchTerm: string = '';
+  sortColumn: keyof User | '' = '';
+  sortDirection: 'asc' | 'desc' = 'asc';
 
   constructor(private userService: UserService) {}
 
@@ -21,7 +25,51 @@ export class UserManagementComponent implements OnInit {
   loadUsers(): void {
     this.userService.getAllUsers().subscribe((data) => {
       this.users = data;
+      this.filteredUsers = [...this.users];
     });
+  }
+
+  applyFilter() {
+    this.filteredUsers = this.users.filter(user =>
+        user.username.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        user.email.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        user.roles.some(role => role.name.toLowerCase().includes(this.searchTerm.toLowerCase()))
+    );
+    this.sort(this.sortColumn);
+  }
+
+  sort(column: keyof User | 'roles' | '') {
+    if (column === this.sortColumn) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+
+    if (column) {
+      this.filteredUsers.sort((a, b) => {
+        if (column === 'roles') {
+          const rolesA = a.roles.map(role => role.name).join(', ');
+          const rolesB = b.roles.map(role => role.name).join(', ');
+          return this.sortDirection === 'asc'
+              ? rolesA.localeCompare(rolesB)
+              : rolesB.localeCompare(rolesA);
+        } else {
+          const valueA = a[column];
+          const valueB = b[column];
+          if (typeof valueA === 'string' && typeof valueB === 'string') {
+            return this.sortDirection === 'asc'
+                ? valueA.localeCompare(valueB)
+                : valueB.localeCompare(valueA);
+          } else if (typeof valueA === 'number' && typeof valueB === 'number') {
+            return this.sortDirection === 'asc'
+                ? valueA - valueB
+                : valueB - valueA;
+          }
+        }
+        return 0;
+      });
+    }
   }
 
   editUser(user: any): void {
@@ -33,4 +81,11 @@ export class UserManagementComponent implements OnInit {
       this.loadUsers();
     });
   }
+
+  getSortIcon(column: string): string {
+    if (column !== this.sortColumn) return '↕';
+    return this.sortDirection === 'asc' ? '↑' : '↓';
+  }
+
+
 }
